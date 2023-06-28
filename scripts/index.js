4d8b8846ea257fdd957e399bc8485b941fc03805
John Glen Falceso <johnglenfalceso@protonmail.com>
2023-06-28 14:08:29 +0800

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+// opening section animation
+/*
+document.addEventListener('scroll', function (e) {
+    var top  = window.scrollY + window.innerHeight,
+        isVisible = top > document.querySelector('#opening_section').offsetTop;
+        
+     if (isVisible) {
+        console.log('yes');
+       document.querySelector('#opening_section h1').classList.add('animate__fadeInDown');
+       document.querySelector('#opening_section img').classList.add('animate__fadeInDown');
+     }
+  });
+*/
